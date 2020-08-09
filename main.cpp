8b14d8e0b3207440780cc0dde96d121af96f97b5
miyasaka <yoyuohlhjl@yahoo.co.jp>
2020-08-10 00:56:10 +0900

    modify main

--- main.cpp
+++ main.cpp
@@ -3,16 +3,20 @@
 #include "aig.hpp"
 
 int main(int argc, char **argv) {
+  extern void Bmc_MeshTest( aigman * p, int X, int Y, int T, int fVerbose, bool inputbuf );
   extern void Bmc_MeshTest2( aigman * p, int X, int Y, int T, int fVerbose );
 
-  if(argc < 2) {
-    std::cout << "specify aigname" << std::endl;
+  if(argc < 4) {
+    std::cout << "usage aigname meshsize cycle" << std::endl;
+    return 1;
   }
   std::string aigname = argv[1];
+  int meshsize = atoi(argv[2]);
+  int cycle = atoi(argv[3]);
   aigman p;
   p.read(aigname);
 
-  Bmc_MeshTest2(&p, 10, 10, 10, 1);
+  Bmc_MeshTest(&p, meshsize, meshsize, cycle, 1, 1);
   
   return 0;
 }
